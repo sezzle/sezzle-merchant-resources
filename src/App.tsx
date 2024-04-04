99634bfd9e7e051109549194bdb73886dfc52933
Allyson Hammerlund <allyson.hammerlund@sezzle.com>
2024-04-04 11:55:52 -0500

    migrate errorBoundary and event to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,26 +15,14 @@ import mobile from "./assets/mobile.svg";
 import fiveStar from "./assets/five-star.svg";
 import Logo from "./components/Logo";
 import { sendEvent } from "./remote/api";
-import {
-    ABOUT_SEZZLE_ONLOAD_EVENT,
-    ABOUT_SEZZLE_ERROR_EVENT,
-} from "./constants";
-import { ErrorBoundary } from "react-error-boundary";
-import { useEffect, ErrorInfo } from "react";
+import { ABOUT_SEZZLE_ONLOAD_EVENT } from "./constants";
 
-let config: AppConfig | undefined;
-
-const dispatchEvent = (
-    config: AppConfig | undefined,
-    eventType: string,
-    description: string
-) => {
+const dispatchEvent = (config: AppConfig | undefined, eventType: string) => {
     const body = [
         {
             event_name: eventType,
             merchant_site: config?.origin,
             merchant_uuid: config?.merchant_uuid,
-            description: description,
         },
     ];
     // hooks are only accessible from inside a JSX element.
@@ -44,231 +32,201 @@ const dispatchEvent = (
 };
 
 const onSuccess = (config: AppConfig | undefined) => {
-    dispatchEvent(config, ABOUT_SEZZLE_ONLOAD_EVENT, "");
-};
-
-const onFailure = (error: Error, info: ErrorInfo) => {
-    console.log(`${error}: ${info.componentStack}`);
-    dispatchEvent(config, ABOUT_SEZZLE_ERROR_EVENT, info.componentStack || "");
+    dispatchEvent(config, ABOUT_SEZZLE_ONLOAD_EVENT);
 };
 
 function App() {
     const ctx = useConfig();
-    config = ctx.config;
+    const config = ctx.config;
     const translation: ITranslation = ctx.translation;
-    useEffect(() => {
-        onSuccess(config);
-    });
+    onSuccess(config);
     return (
-        <ErrorBoundary
-            fallback={
-                <div>
-                    Something went wrong.{" "}
-                    <a
-                        href="https://sezzle.com/how-it-works/"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        Open Sezzle in new tab.
-                    </a>
-                </div>
-            }
-            onError={onFailure}
+        <div
+            className={`sezzle-container ${
+                config && config.theme !== DEFAULT_THEME
+                    ? "sezzle-container-dark"
+                    : ""
+            }`}
         >
-            <div
-                className={`sezzle-container ${
-                    config && config.theme !== DEFAULT_THEME
-                        ? "sezzle-container-dark"
-                        : ""
-                }`}
-            >
-                <div
-                    className="sezzle-logo"
-                    aria-label={translation.logoAltText}
-                >
-                    <Logo />
+            <div className="sezzle-logo" aria-label={translation.logoAltText}>
+                <Logo />
+            </div>
+            <h1 className="sezzle-header">{translation.header}</h1>
+            <p className="sezzle-description">
+                {translation.description}
+                <sup>1</sup>
+            </p>
+            <div className="payment-pie-area">
+                <div className="installment-wrapper">
+                    <div className="pie">
+                        <img src={quarterPie} alt=""></img>
+                    </div>
+                    <div className="installment">25%</div>
+                    <div className="due-date">{translation.today}</div>
                 </div>
-                <h1 className="sezzle-header">{translation.header}</h1>
-                <p className="sezzle-description">
-                    {translation.description}
-                    <sup>1</sup>
-                </p>
-                <div className="payment-pie-area">
-                    <div className="installment-wrapper">
-                        <div className="pie">
-                            <img src={quarterPie} alt=""></img>
-                        </div>
-                        <div className="installment">25%</div>
-                        <div className="due-date">{translation.today}</div>
-                    </div>
-                    <div className="pie-separator">
-                        <img src={pieSeparator} alt=""></img>
-                    </div>
-                    <div className="installment-wrapper">
-                        <div className="pie">
-                            <img src={halfPie} alt=""></img>
-                        </div>
-                        <div className="installment">25%</div>
-                        <div className="due-date">2 {translation.weeks}</div>
-                    </div>
-                    <div className="pie-separator">
-                        <img src={pieSeparator} alt=""></img>
-                    </div>
-                    <div className="installment-wrapper">
-                        <div className="pie">
-                            <img src={threeQuarterPie} alt=""></img>
-                        </div>
-                        <div className="installment">25%</div>
-                        <div className="due-date">4 {translation.weeks}</div>
-                    </div>
-                    <div className="pie-separator">
-                        <img src={pieSeparator} alt=""></img>
-                    </div>
-                    <div className="installment-wrapper">
-                        <div className="pie">
-                            <img src={fullPie} alt=""></img>
-                        </div>
-                        <div className="installment">25%</div>
-                        <div className="due-date">6 {translation.weeks}</div>
-                    </div>
+                <div className="pie-separator">
+                    <img src={pieSeparator} alt=""></img>
                 </div>
-                <div className="section-separator"></div>
-
-                <h2 className="sezzle-subheader">{translation.subHeader}</h2>
-                <p className="sezzle-description">
-                    {translation.subDescription}
-                </p>
-
-                <div className="card-area">
-                    <div className="info-card">
-                        <div className="shopping-cart-icon">
-                            <img src={cartIcon} alt=""></img>
-                        </div>
-                        <p className="sezzle-info">{translation.cartInfo}</p>
+                <div className="installment-wrapper">
+                    <div className="pie">
+                        <img src={halfPie} alt=""></img>
                     </div>
-                    <div className="info-card">
-                        <div className="sezzle-card-icon">
-                            <img src={cardIcon} alt=""></img>
-                        </div>
-                        <p className="sezzle-info">
-                            {translation.checkoutInfo}
-                        </p>
-                    </div>
-                    <div className="info-card">
-                        <div className="ship-time-icon">
-                            <img src={shipTimeIcon} alt=""></img>
-                        </div>
-                        <p
-                            className="sezzle-info"
-                            dangerouslySetInnerHTML={{
-                                __html: translation.shipmentInfo,
-                            }}
-                        ></p>
-                    </div>
+                    <div className="installment">25%</div>
+                    <div className="due-date">2 {translation.weeks}</div>
                 </div>
-
-                <div className="section-separator"></div>
-
-                <div className="cta-area">
-                    <div className="cta-main">
-                        <h3 className="cta-header">{translation.ctaHeader}</h3>
-                        <p className="cta-description">
-                            {translation.ctaDescription}
-                        </p>
-                        <a
-                            className="cta-button"
-                            href="https://sezzle.com/app"
-                            rel="noreferrer"
-                            target="_blank"
-                        >
-                            {translation.ctaButton}
-                        </a>
-                    </div>
-                    <div className="mobile-reviews">
-                        <div className="mobile-app-img">
-                            <img src={mobile} alt=""></img>
-                        </div>
-                        <div className="review-card-area">
-                            <div className="review-card review-card-1">
-                                <div className="trustpilot-group">
-                                    <div className="trustpilot">
-                                        <img src={trustPilot} alt=""></img>
-                                    </div>
-                                    <div
-                                        className="five-stars"
-                                        aria-label="Five stars"
-                                    >
-                                        <img src={fiveStar} alt=""></img>
-                                    </div>
-                                </div>
-                                <h4 className="review-header">
-                                    {translation.review1Header}
-                                </h4>
-                                <p className="review-description">
-                                    {translation.review1Description}
-                                </p>
-                                <div className="review-name">
-                                    {translation.reviewer1Name}{" "}
-                                    <span className="review-date">
-                                        {translation.review1Date}
-                                    </span>
-                                </div>
-                            </div>
-
-                            <div className="review-card review-card-2">
-                                <div className="trustpilot-group">
-                                    <div className="trustpilot">
-                                        <img src={trustPilot} alt=""></img>
-                                    </div>
-                                    <div
-                                        className="five-stars"
-                                        aria-label="{translation.ratingAltTex}"
-                                    >
-                                        <img src={fiveStar} alt=""></img>
-                                    </div>
-                                </div>
-                                <h4 className="review-header">
-                                    {translation.review2Header}
-                                </h4>
-                                <p className="review-description">
-                                    {translation.review2Header}
-                                </p>
-                                <div className="review-name">
-                                    {translation.reviewer2Name}
-                                    <span className="review-date">
-                                        {translation.review2Date}
-                                    </span>
-                                </div>
-                            </div>
-                            <div className="review-card review-card-3">
-                                <div className="trustpilot-group">
-                                    <div className="trustpilot">
-                                        <img src={trustPilot} alt=""></img>
-                                    </div>
-                                    <div
-                                        className="five-stars"
-                                        aria-label={translation.ratingAltText}
-                                    >
-                                        <img src={fiveStar} alt=""></img>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div className="pie-separator">
+                    <img src={pieSeparator} alt=""></img>
                 </div>
-
-                <div className="terms">
-                    <p>
-                        <sup>1</sup>
-                        {translation.term1}
-                    </p>
-                    <p>
-                        <sup>2</sup>
-                        {translation.term2}
-                    </p>
+                <div className="installment-wrapper">
+                    <div className="pie">
+                        <img src={threeQuarterPie} alt=""></img>
+                    </div>
+                    <div className="installment">25%</div>
+                    <div className="due-date">4 {translation.weeks}</div>
+                </div>
+                <div className="pie-separator">
+                    <img src={pieSeparator} alt=""></img>
+                </div>
+                <div className="installment-wrapper">
+                    <div className="pie">
+                        <img src={fullPie} alt=""></img>
+                    </div>
+                    <div className="installment">25%</div>
+                    <div className="due-date">6 {translation.weeks}</div>
                 </div>
             </div>
-        </ErrorBoundary>
+            <div className="section-separator"></div>
+
+            <h2 className="sezzle-subheader">{translation.subHeader}</h2>
+            <p className="sezzle-description">{translation.subDescription}</p>
+
+            <div className="card-area">
+                <div className="info-card">
+                    <div className="shopping-cart-icon">
+                        <img src={cartIcon} alt=""></img>
+                    </div>
+                    <p className="sezzle-info">{translation.cartInfo}</p>
+                </div>
+                <div className="info-card">
+                    <div className="sezzle-card-icon">
+                        <img src={cardIcon} alt=""></img>
+                    </div>
+                    <p className="sezzle-info">{translation.checkoutInfo}</p>
+                </div>
+                <div className="info-card">
+                    <div className="ship-time-icon">
+                        <img src={shipTimeIcon} alt=""></img>
+                    </div>
+                    <p
+                        className="sezzle-info"
+                        dangerouslySetInnerHTML={{
+                            __html: translation.shipmentInfo,
+                        }}
+                    ></p>
+                </div>
+            </div>
+
+            <div className="section-separator"></div>
+
+            <div className="cta-area">
+                <div className="cta-main">
+                    <h3 className="cta-header">{translation.ctaHeader}</h3>
+                    <p className="cta-description">
+                        {translation.ctaDescription}
+                    </p>
+                    <a
+                        className="cta-button"
+                        href="https://sezzle.com/app"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        {translation.ctaButton}
+                    </a>
+                </div>
+                <div className="mobile-reviews">
+                    <div className="mobile-app-img">
+                        <img src={mobile} alt=""></img>
+                    </div>
+                    <div className="review-card-area">
+                        <div className="review-card review-card-1">
+                            <div className="trustpilot-group">
+                                <div className="trustpilot">
+                                    <img src={trustPilot} alt=""></img>
+                                </div>
+                                <div
+                                    className="five-stars"
+                                    aria-label="Five stars"
+                                >
+                                    <img src={fiveStar} alt=""></img>
+                                </div>
+                            </div>
+                            <h4 className="review-header">
+                                {translation.review1Header}
+                            </h4>
+                            <p className="review-description">
+                                {translation.review1Description}
+                            </p>
+                            <div className="review-name">
+                                {translation.reviewer1Name}{" "}
+                                <span className="review-date">
+                                    {translation.review1Date}
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="review-card review-card-2">
+                            <div className="trustpilot-group">
+                                <div className="trustpilot">
+                                    <img src={trustPilot} alt=""></img>
+                                </div>
+                                <div
+                                    className="five-stars"
+                                    aria-label="{translation.ratingAltTex}"
+                                >
+                                    <img src={fiveStar} alt=""></img>
+                                </div>
+                            </div>
+                            <h4 className="review-header">
+                                {translation.review2Header}
+                            </h4>
+                            <p className="review-description">
+                                {translation.review2Header}
+                            </p>
+                            <div className="review-name">
+                                {translation.reviewer2Name}
+                                <span className="review-date">
+                                    {translation.review2Date}
+                                </span>
+                            </div>
+                        </div>
+                        <div className="review-card review-card-3">
+                            <div className="trustpilot-group">
+                                <div className="trustpilot">
+                                    <img src={trustPilot} alt=""></img>
+                                </div>
+                                <div
+                                    className="five-stars"
+                                    aria-label={translation.ratingAltText}
+                                >
+                                    <img src={fiveStar} alt=""></img>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="terms">
+                <p>
+                    <sup>1</sup>
+                    {translation.term1}
+                </p>
+                <p>
+                    <sup>2</sup>
+                    {translation.term2}
+                </p>
+            </div>
+        </div>
     );
 }
 
