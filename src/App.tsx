9f583e0dbdd9da02b8e0100eec1f0a8d4ad925c4
sitikanthamallik <sitikantha.mallik@sezzle.com>
2025-02-21 03:38:46 +0530

    adding proper div tag

--- src/App.tsx
+++ src/App.tsx
@@ -221,9 +221,9 @@ function App() {
         <p>
           <sup>1</sup>
           {countryCodeRef.current === "CA" ? (
-              <><span id="term3">{translation.term1}</span><span id="term3">{translation.term3}</span></>
+             <div className="CAterms"><span id="term3">{translation.term1}</span><span id="term3">{translation.term3}</span></div>
           ) : (
-              <span id="term2">{translation.term2}</span>
+             <div className="USterms"><span id="term2">{translation.term2}</span></div>
           )}
         </p>
         <p>
